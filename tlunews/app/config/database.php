26ab71f030891cf4bd0b0f5bd4de01f39dd2445a
hoanpham213 <144581454+hoanpham213@users.noreply.github.com>
2024-12-07 20:31:32 +0700

    Add files via upload

---
+++ tlunews/app/config/database.php
@@ -0,0 +1,22 @@
+<?php
+define('APP_ROOT', dirname(__FILE__,3));
+class Database {
+    private $host = 'localhost';
+    private $db = 'tintuc';
+    private $user = 'root';
+    private $pass = '';
+    public $conn;
+
+    public function getConnect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Kết nối thành công !";
+        } catch (PDOException $e) {
+            echo "Kết nối thất bại: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
---
+++ tlunews/index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('../tlunews/configs/database.php');
+
+    $database = new Database();
+    $database->getConnect();
+?>
